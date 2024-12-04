db059b0a797e6c4d53f61490b767c874134dd4fb
hyeon1003 <lsnn6096@gmail.com>
2024-12-04 13:52:13 +0900

    vision.hpp

--- vision.hpp
+++ vision.hpp
@@ -9,6 +9,6 @@ using namespace cv;
 void preprocess(VideoCapture& source, Mat& frame, Mat& gray, Mat& thresh);// 전처리 함수: 입력 영상을 그레이스케일로 변환하고 이진화 및 자르기 처리
 void findObjects(const Mat& thresh, Point& tmp_pt, Mat& result, Mat& stats, Mat& centroids);// 라인 후보 객체를 찾는 함수
 void drawObjects(const Mat& stats, const Mat& centroids, const Point& tmp_pt, Mat& result);// 객체를 표시하는 함수
-int getError(const Mat& result, const Point& tmp_pt);// 에러 계산 함수: 라인의 중심과 영상 중심의 차이를 반환
+double getError(const Mat& frame, const Point& po, double gain);// 에러 계산 함수: 라인의 중심과 영상 중심의 차이를 반환
 
 #endif
